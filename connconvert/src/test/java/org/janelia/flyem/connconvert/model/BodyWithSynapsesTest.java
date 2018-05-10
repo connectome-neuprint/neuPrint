ba468e8559763b1674938e5bcb291b01ac025aed
neubarthn <nicoleneubarth@gmail.com>
2018-05-10 11:07:22 -0400

    testing equals and hashcode for bodywithsynapses

--- connconvert/src/test/java/org/janelia/flyem/connconvert/model/BodyWithSynapsesTest.java
+++ connconvert/src/test/java/org/janelia/flyem/connconvert/model/BodyWithSynapsesTest.java
@@ -31,6 +31,32 @@ public class BodyWithSynapsesTest {
         // TODO: improve this test
     }
 
+    @Test
+    public void testEqualsAndHashCode() {
+
+        final List<BodyWithSynapses> parsedBodyList = BodyWithSynapses.fromJson(BODY_LIST_JSON);
+
+        BodyWithSynapses body1 = new BodyWithSynapses(new Long(85), parsedBodyList.get(0).getSynapseSet());
+        BodyWithSynapses body2 = new BodyWithSynapses(new Long(85), parsedBodyList.get(1).getSynapseSet());
+        BodyWithSynapses body3 = new BodyWithSynapses(new Long(85), parsedBodyList.get(2).getSynapseSet());
+
+        //reflexive
+        Assert.assertTrue(body1.equals(body1));
+        //symmetric
+        Assert.assertTrue(body1.equals(body2) && body2.equals(body1));
+        //transitive
+        Assert.assertTrue(body1.equals(body2)  && body2.equals(body3) && body3.equals(body1));
+        //consistent
+        Assert.assertTrue(body2.equals(body1) && body2.equals(body1));
+        //not equal to null
+        Assert.assertTrue(!body2.equals(null));
+
+        Assert.assertNotSame(body1, body2);
+        Assert.assertTrue(body1.hashCode() == body2.hashCode());
+
+
+    }
+
     // TODO: add more tests!
 
     private static final String BODY_LIST_JSON =
@@ -44,25 +70,25 @@ public class BodyWithSynapsesTest {
             "          [ 4298, 2294, 1542 ], [ 4301, 2276, 1535 ], [ 4292, 2261, 1542 ]\n" +
             "        ]\n" +
             "      },\n" +
-            "      { \"Type\": \"post\", \"Location\": [ 4222, 2402, 1688 ], \"ConnectsFrom\": [ [ 4236, 2394, 1700 ] ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ] }\n" +
+            "      { \"Type\": \"post\", \"Location\": [ 4222, 2402, 1688 ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ], \"ConnectsFrom\": [ [ 4236, 2394, 1700 ] ]  }\n" +
             "    ]\n" +
             "  },\n" +
             "  {\n" +
             "    \"BodyId\": 26311,\n" +
             "    \"SynapseSet\": [\n" +
-            "      { \"Type\": \"post\", \"Location\": [ 4301, 2276, 1535 ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ] }\n" +
+            "      { \"Type\": \"post\", \"Location\": [ 4301, 2276, 1535 ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ] }\n" +
             "    ]\n" +
             "  },\n" +
             "  {\n" +
             "    \"BodyId\": 2589725,\n" +
             "    \"SynapseSet\": [\n" +
-            "      { \"Type\": \"post\", \"Location\": [ 4298, 2294, 1542 ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ] }\n" +
+            "      { \"Type\": \"post\", \"Location\": [ 4298, 2294, 1542 ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ] }\n" +
             "    ]\n" +
             "  },\n" +
             "  {\n" +
             "    \"BodyId\": 831744,\n" +
             "    \"SynapseSet\": [\n" +
-            "      { \"Type\": \"post\", \"Location\": [ 4292, 2261, 1542 ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ] }\n" +
+            "      { \"Type\": \"post\", \"Location\": [ 4292, 2261, 1542 ], \"Confidence\": 1.0, \"rois\": [ \"seven_column_roi\" ], \"ConnectsFrom\": [ [ 4287, 2277, 1542 ] ] }\n" +
             "    ]\n" +
             "  }\n" +
             "]";
