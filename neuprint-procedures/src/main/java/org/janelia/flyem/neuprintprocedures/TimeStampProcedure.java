f019d6923d592601f47de27d4620dd8956847eab
neubarthn <nicoleneubarth@gmail.com>
2018-06-19 15:54:30 -0400

    disabled time stamp stored procedure

--- neuprint-procedures/src/main/java/org/janelia/flyem/neuprintprocedures/TimeStampProcedure.java
+++ neuprint-procedures/src/main/java/org/janelia/flyem/neuprintprocedures/TimeStampProcedure.java
@@ -2,31 +2,11 @@ package org.janelia.flyem.neuprintprocedures;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.logging.Log;
-import org.neo4j.procedure.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public class TimeStampProcedure {
-
-    @Context
-    public GraphDatabaseService dbService;
-
-    @Context
-    public Log log;
-
-
-    @Procedure(value = "neuPrintProcedures.timeStamp", mode = Mode.WRITE)
-    @Description("For the node with the given node ID, add time stamp property with current date")
-    public void timeStamp(@Name("nodeId") long nodeId) {
-
-        Node node = dbService.getNodeById(nodeId);
-
-        node.setProperty("timeStamp", LocalDate.now());
-
-    }
 
     //for testing in embedded mode or registering transaction events
     public static void timeStampEmbedded(Set<Long> nodeIdSet, GraphDatabaseService dbService) {
--- neuprint-procedures/src/test/java/org/janelia/flyem/neuprintprocedures/TimeStampTest.java
+++ neuprint-procedures/src/test/java/org/janelia/flyem/neuprintprocedures/TimeStampTest.java
@@ -7,46 +7,17 @@ import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.harness.junit.Neo4jRule;
-import org.neo4j.test.TestGraphDatabaseFactory;
 
 import java.time.LocalDate;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static org.neo4j.driver.v1.Values.parameters;
-import static org.neo4j.graphdb.Label.label;
 
 public class TimeStampTest {
 
     @Rule
     public Neo4jRule neo4j = new Neo4jRule()
             .withProcedure(TimeStampProcedure.class);
-
-    @Test
-    public void shouldApplyTimeStampAfterCall() {
-
-        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withoutEncryption().toConfig())) {
-
-            Session session = driver.session();
-
-            Long nodeId = session.run("CREATE (n:Neuron:test{id:1}) RETURN id(n)").single().get(0).asLong();
-
-            session.run("MATCH (n:Neuron{id:1}) SET n.timeStamp=$yesterday", parameters("yesterday", LocalDate.of(2000, 1, 1)));
-
-            session.run("CALL neuPrintProcedures.timeStamp($nodeId)", parameters("nodeId", nodeId));
-
-            LocalDate timeStamp = session.run("MATCH (n:Neuron{id:1}) RETURN n.timeStamp").single().get(0).asLocalDate();
-
-            Assert.assertEquals(LocalDate.now(), timeStamp);
-
-        }
-    }
-
 
     @Test
     public void shouldAddTimeStampUponCreation() {
@@ -259,6 +230,27 @@ public class TimeStampTest {
 
         }
 
+    }
+
+    @Test
+    public void shouldNotAddTimeStampIfReadOnlyQuery() {
+        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build().withoutEncryption().toConfig())) {
+
+            Session session = driver.session();
+
+            session.run("CREATE (n{id:1})  \n" +
+                    "CREATE (m{id:2}) \n" +
+                    "CREATE (n)-[:RelatesTo]->(m)");
+
+            session.run("MATCH (n) SET n.timeStamp=$yesterday",parameters("yesterday",LocalDate.of(2000,1,1)));
+
+            session.run("MATCH (n{id:1}) RETURN n");
+
+            LocalDate timeStamp = session.run("MATCH (n{id:1}) RETURN n.timeStamp").single().get(0).asLocalDate();
+
+            Assert.assertEquals(LocalDate.of(2000,1,1),timeStamp);
+
+        }
     }
 }
 
