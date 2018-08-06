28f92ba9911e0a3db30b1e86982a9bfa1004828e
neubarthn <nicoleneubarth@gmail.com>
2018-08-06 13:24:43 -0400

    created CleaveAction class to read cleaveAction jsons

--- neuprint-procedures/src/main/java/org/janelia/flyem/neuprintprocedures/CleaveAction.java
+++ neuprint-procedures/src/main/java/org/janelia/flyem/neuprintprocedures/CleaveAction.java
@@ -1,4 +1,65 @@
 package org.janelia.flyem.neuprintprocedures;
 
+import com.google.gson.annotations.SerializedName;
+import org.janelia.flyem.neuprinter.model.Synapse;
+
+import java.util.Set;
+
 public class CleaveAction {
+
+    @SerializedName("NewBodyId")
+    private Long newBodyId;
+
+    @SerializedName("OrigBodyId")
+    private Long originalBodyId;
+
+    @SerializedName("NewBodySize")
+    private Long newBodySize;
+
+    @SerializedName("NewBodySynapses")
+    private Set<Synapse> newBodySynapses;
+
+    public Set<Synapse> getNewBodySynapses() {
+        return newBodySynapses;
+    }
+
+    public Long getNewBodyId() {
+        return newBodyId;
+    }
+
+    public Long getNewBodySize() {
+        return newBodySize;
+    }
+
+    public Long getOriginalBodyId() {
+        return originalBodyId;
+    }
+
+    @Override
+    public String toString() {
+        return "newBodyId: " + this.newBodyId +
+                ", originalBodyId: " + this.originalBodyId +
+                ", newBodySize: " + this.newBodySize +
+                ", newBodySynapses: " + this.newBodySynapses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean isEqual = false;
+        if (this == o) {
+            isEqual = true;
+        } else if (o instanceof CleaveAction) {
+            final CleaveAction that = (CleaveAction) o;
+            isEqual = this.newBodyId.equals(that.newBodyId) && this.originalBodyId.equals(that.originalBodyId);
+        }
+        return isEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + this.newBodyId.hashCode();
+        result = 31 * result + this.originalBodyId.hashCode();
+        return result;
+    }
 }
